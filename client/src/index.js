27e667b1c582c5407af3d7106ce0d81e79608502
Prathibha Ratnayake <prsthibha@gmail.com>
2022-12-12 12:32:03 +0530

    Setup redux state

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,23 @@
+import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
+import globalReducer from 'state';
 import App from './App';
+import './index.css';
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
