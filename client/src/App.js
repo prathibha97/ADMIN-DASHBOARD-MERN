8c27da824957e4b26ef07e923ae542ea6924fe2e
Prathibha Ratnayake <prsthibha@gmail.com>
2022-12-13 12:14:37 +0530

    Add Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
-import { CssBaseline, ThemeProvider } from "@mui/material"
-import { createTheme } from "@mui/material/styles"
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import Dashboard from "pages/Dashboard";
+import Layout from "pages/Layout";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
-import { themeSettings } from "theme"
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+import { themeSettings } from "theme";
 
 function App() {
-  const mode = useSelector((state)=>state.global.mode)
-  const theme = useMemo(()=>createTheme(themeSettings(mode)), [mode])
+  const mode = useSelector((state) => state.global.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline/>
-      </ThemeProvider>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to={`/dashboard`} replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </Router>
     </div>
   );
 }
