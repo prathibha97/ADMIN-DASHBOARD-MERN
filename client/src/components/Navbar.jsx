dbe236115b3e19d1d595686127938052406a56a7
Prathibha Ratnayake <prsthibha@gmail.com>
2022-12-13 13:06:33 +0530

    Add Sidebar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,54 +1,118 @@
-import React from 'react'
-import { LightModeOutlined, DarkModeOutlined, Menu as MenuIcon, Search, SettingsOutlined, ArrowDropDownOutlined } from '@mui/icons-material'
-import FlexBetween from './FlexBetween'
-import { useDispatch } from 'react-redux'
-import { setMode } from 'state'
-import ProfileImage from 'assets/profile.jpeg'
-import { AppBar, IconButton, InputBase, Toolbar, useTheme } from '@mui/material'
+import {
+  ArrowDropDownOutlined, DarkModeOutlined, LightModeOutlined, Menu as MenuIcon,
+  Search,
+  SettingsOutlined
+} from "@mui/icons-material";
+import {
+  AppBar, Box, Button, IconButton,
+  InputBase, Menu,
+  MenuItem, Toolbar, Typography, useTheme
+} from "@mui/material";
+import profileImage from "assets/profile.jpeg";
+import FlexBetween from "components/FlexBetween";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
 
-const Navbar = () => {
-  const dispatch = useDispatch()
-  const theme = useTheme()
+const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
+  const dispatch = useDispatch();
+  const theme = useTheme();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isOpen = Boolean(anchorEl);
+  const handleClick = (event) => setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
 
   return (
     <AppBar
-    sx={{position: 'static', background:'none', boxShadow:'none'}}
+      sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+      }}
     >
-      <Toolbar sx={{justifyContent:'space-between'}}>
-        {/* left side */}
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        {/* LEFT SIDE */}
         <FlexBetween>
-          <IconButton onClick={()=>console.log('open/close sidebar')}>
-            <MenuIcon/>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+            <MenuIcon />
           </IconButton>
-        <FlexBetween
-         backgroundColor={theme.palette.background.alt}
-         borderRadius='9px'
-         gap='3rem'
-         p='0.1rem 1.5rem'
-         >
-          <InputBase placeholder='Search...'>
-          <IconButton>
-            <Search />
-          </IconButton>
-          </InputBase>
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
         </FlexBetween>
-        </FlexBetween>
-        {/* right side */}
-        <FlexBetween gap='1.5rem'>
-          <IconButton onClick={()=>dispatch(setMode())}>
-            {theme.palette.mode === 'dark'? (
-              <DarkModeOutlined sx={{fontSize:'25px'}}/>
+
+        {/* RIGHT SIDE */}
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
             ) : (
-              <LightModeOutlined sx={{fontSize:'25px'}}/>
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
             )}
           </IconButton>
           <IconButton>
-            <SettingsOutlined/>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
           </IconButton>
+
+          <FlexBetween>
+            <Button
+              onClick={handleClick}
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                textTransform: "none",
+                gap: "1rem",
+              }}
+            >
+              <Box
+                component="img"
+                alt="profile"
+                src={profileImage}
+                height="32px"
+                width="32px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.85rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                </Typography>
+                <Typography
+                  fontSize="0.75rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                </Typography>
+              </Box>
+              <ArrowDropDownOutlined
+                sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
+              />
+            </Button>
+            <Menu
+              anchorEl={anchorEl}
+              open={isOpen}
+              onClose={handleClose}
+              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            >
+              <MenuItem onClick={handleClose}>Log Out</MenuItem>
+            </Menu>
+          </FlexBetween>
         </FlexBetween>
       </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
