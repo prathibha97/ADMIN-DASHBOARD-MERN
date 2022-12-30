a5326b93630503afa91a8b4f754548de5dcf3edf
Prathibha Ratnayake <prsthibha@gmail.com>
2022-12-30 09:33:39 +0530

    Add getUser route & controller

--- server/routes/general.routes.js
+++ server/routes/general.routes.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import {getUser} from '../controllers/general.controller.js'
 
 const generalRouter = express.Router()
+
+generalRouter.get('/user/:id', getUser)
 
 
 export default generalRouter
