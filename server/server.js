806566925ea04332cbecd0820c8655f931a15507
Prathibha Ratnayake <prsthibha@gmail.com>
2022-12-30 10:49:07 +0530

    Add seeder data

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import http from 'http'
 import app from './app.js'
 import { User } from './models/User.js'
+import { Product } from './models/Product.js'
+import { ProductStat } from './models/ProductStat.js'
 import connectDB from './services/mongo.js'
-import {dataUser} from './data/index.js'
+import {dataUser, dataProduct, dataProductStat} from './data/index.js'
 
 const server = http.createServer(app)
 const port =  process.env.PORT | 5000
@@ -11,4 +13,6 @@ server.listen(port, ()=>{
   console.log(`Server listening on port ${port}`)
   connectDB()
   // User.insertMany(dataUser)
+  // Product.insertMany(dataProduct)
+  // ProductStat.insertMany(dataProductStat)
 })
