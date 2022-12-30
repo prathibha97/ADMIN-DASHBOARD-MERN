7aefe9d19cb62c6671b044035320c8bc16735f18
Prathibha Ratnayake <prsthibha@gmail.com>
2022-12-30 10:44:18 +0530

    Add product & productStat schema

---
+++ server/models/Product.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+  category: String,
+  rating: Number,
+  supply: Number,
+}, { timestamps: true })
+
+export const Product = mongoose.model('Product', productSchema)
