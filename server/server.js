18d97158aa27b84ae125516d121443f84152acdf
Prathibha Ratnayake <prsthibha@gmail.com>
2022-12-30 09:33:14 +0530

    Insert seeder data

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import http from 'http'
 import app from './app.js'
+import { User } from './models/User.js'
 import connectDB from './services/mongo.js'
+import {dataUser} from './data/index.js'
 
 const server = http.createServer(app)
 const port =  process.env.PORT | 5000
@@ -8,4 +10,5 @@ const port =  process.env.PORT | 5000
 server.listen(port, ()=>{
   console.log(`Server listening on port ${port}`)
   connectDB()
+  // User.insertMany(dataUser)
 })
